3075a5033206203d77f5e75632fe573b06e2aabd
MaheKarim <mahekarim@gmail.com>
2019-09-10 18:19:25 +0600

    Verify Off

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,8 +13,11 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware(['auth'=> 'verified']);
+        $this->middleware('auth');
+      //  $this->middleware(['auth'=> 'verified']);
+
     }
+
 
     /**
      * Show the application dashboard.
