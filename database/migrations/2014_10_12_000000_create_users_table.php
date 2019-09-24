993b9a5b2f4c275da3d29bd5727ea06f94ef2609
MaheKarim <mahekarim@gmail.com>
2019-09-24 18:07:50 +0600

    Migrate sucessfully!

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('phn_number')>union()->nullable();
+            $table->string('phn_number')->nullable();
             $table->integer('user_role')->nullable();
             $table->integer('balance')->default(0);
             $table->timestamp('email_verified_at')->nullable();
