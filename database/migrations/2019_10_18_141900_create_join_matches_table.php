a2b656eeeede3c6d4a76e084c416bb78aff0f146
Mahekarim <mahekarim@gmail.com>
2019-10-18 20:24:46 +0600

    Join Match ID Create

---
+++ database/migrations/2019_10_18_141900_create_join_matches_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJoinMatchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('join_matches', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id');
+            $table->integer('team_id');
+            $table->integer('full_match_setup_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('join_matches');
+    }
+}
